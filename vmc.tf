c816669c0b8401e2d372d95031d951d831a67719
adityabharath <49548932+adityabharath@users.noreply.github.com>
2019-05-08 09:33:24 +0530

    Update vmc.tf

--- vmc.tf
+++ vmc.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "main" {
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = "$"
+  location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
 }
 
